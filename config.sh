a6619a9d8d77a198791a45d7aef17cfbe5d6ff8e
itznokx <dimitri.b.medeiros@gmail.com>
2025-09-03 01:36:24 -0300

    fix config.sh ( sync with dot_files repo )

--- config.sh
+++ config.sh
@@ -1,12 +1,3 @@
-# Configure shell script (AUTO INSTALL PLUGINS)
-
-# First of all: PACKER
-echo "Copying packer repo" 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-echo "Install ripgrep"
-sudo pacman -S ripgrep
-
+#!/bin/bash
 echo "Sync Packer plugins"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerInstall'
